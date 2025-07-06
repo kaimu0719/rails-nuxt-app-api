96b6c3c0ad94ef524d09d2c606e4e419da974122
KaimuSeino <skim07a19yki@icloud.com>
2025-07-06 11:24:51 +0900

    各コントローラにCookieを扱えるように`ActionController::Cookies`をincludeし、認可を行うためのモジュールと、CSRF対策を行うためのXHRのチェックを行う処理を追加。またサーバーエラー時の共通レスポンスを定義。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+  # ActionController::Cookiesをincludeして、Cookieの操作を可能にする
+  #【cookiesメソッドについて】https://api.rubyonrails.org/classes/ActionDispatch/Cookies.html
+  include ActionController::Cookies
+
+  # 認可を行う
+  include UserAuthenticateService
+
+  # CSRF対策
+  # 全てのアクションが実行される前に、リクエストヘッダーが、
+  # X-Requested-With: 'XMLHttpRequest' であることを確認する
+  # XMLHttpRequestリクエストでない場合は、403 Forbiddenを返す
+  #
+  # これで得られるCSRF対策
+  # - 悪意のあるサイトからのリクエストを防ぐ（別オリジンのサイトからはカスタムヘッダーを送信できないため）
+  before_action :xhr_request?
+
+  private
+    # XHRリクエストかどうかを確認する
+    def xhr_request?
+      # リクエストヘッダー X-Requested-With: 'XMLHttpRequest' の存在を判定
+      return if request.xml_http_request?
+      render status: :forbidden, json: { status: 403, error: 'Forbidden' }
+    end
+
+    # サーバーエラーのレスポンスを返す
+    def response_500(msg = "Internal Server Error")
+      render status: :internal_server_error, json: { status: 500, error: msg }
+    end
 end
