b04b0199fab13703ac763b7fb39fc67037ed637c
KaimuSeino <skim07a19yki@icloud.com>
2025-07-06 23:00:59 +0900

    ユーザー認証関連のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
     namespace :v1 do
       # users_controller
       resources :users, only: [:index]
+
+      # auth_token_controller
+      resources :auth_token, only: [:create] do
+        # /auth_token/refresh, auth_token/destroyのルーティングを追加
+        collection do
+          post :refresh
+          delete :destroy
+        end
+      end
     end
   end
 end
